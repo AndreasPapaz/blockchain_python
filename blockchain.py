9182a4708d1dd89ab84873835e835fa016a0e228
Andreas <AndreasPapaz@gmail.com>
2017-10-22 19:48:56 -0500

    step 2

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,34 @@
 import hashlib
 import json
+
 from time import time
+from uuid import uuid4
 
 
 class  Blockchain(object):
+
 	def __init__(self):
 		self.chain = []
 		self.current_transactions = []
 
+		self.new_block(previous_hash=1, proof=100)
+
+
 	def new_block(self):
 
-		pass
+		block = {
+			'index': len(self.chain) + 1,
+			'timestamp': time(),
+			'transactions': sekf.current_transactions,
+			'proof': proof,
+			'previous_hash': previous_hash or self.hash(self.chain[-1]),
+		}
+
+		self.current_transactions = []
+
+		self.chain_transacions = []
+		return block
+
 
 	def new_transaction(self, recipient, amount):
 
@@ -22,28 +40,29 @@ class  Blockchain(object):
 
 		return self.last_block['index'] + 1
 
+
+	def proof_of_work(self, last_proof):
+
+		proof = 0
+		while self.valid_proof(last_proof, proof) is False:
+			proof += 1
+
+		return proof
+
+
 	@staticmethod
 	def hash(block):
 
-		pass
+		block_string = json.dumps(block, sort_keys=True).encode()
+		return hashlib.sha256(block_string).hexdigest()
+
+
+	def valid_proof(last_proof, proof):
+		guess = f'{last_proof}{proof}'.encode()
+		guess_hash = hashlib.sha256(guess).hexdigest()
+		return guess_hash[:4] == '0000'
 
 	@property
 	def last_block(self):
 
 		pass
-
-
-
-# block = {
-#     'index': 1,
-#     'timestamp': 1506057125.900785,
-#     'transactions': [
-#         {
-#             'sender': "8527147fe1f5426f9dd545de4b27ee00",
-#             'recipient': "a77f5cdfa2934df3954a5c7c7da5df1f",
-#             'amount': 5,
-#         }
-#     ],
-#     'proof': 324984774000,
-#     'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
-# }
